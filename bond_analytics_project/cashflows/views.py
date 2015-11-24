a8669af4f37eb27346e65369b6a5a2298b360086
bsmukasa <bsmukasa@gmail.com>
2015-11-24 18:43:07 -0500

    Add AddBondView class.

--- bond_analytics_project/cashflows/views.py
+++ bond_analytics_project/cashflows/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic import View
+from bond_analytics_project.cashflows.models import Bond
+
 
 # Create your views here.
+class AddBondView(View):
+    model = Bond
