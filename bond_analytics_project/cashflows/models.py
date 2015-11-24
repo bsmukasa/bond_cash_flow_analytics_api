5c1e53a6518105c30ad3c1c17716126b2917d05f
bsmukasa <bsmukasa@gmail.com>
2015-11-24 18:40:12 -0500

    Minor update to Bond model.

--- bond_analytics_project/cashflows/models.py
+++ bond_analytics_project/cashflows/models.py
@@ -15,8 +15,8 @@ class Bond(models.Model):
         face_value: The principal of the bond to be repaid at the end of the maturity period.
         maturity: Number of periods to maturity is related to the Coupon Payment Frequency.
         coupon_rate: The stated annual interest rate payments for a Bond.
-        bond_price: The current price of the bond in the market.
         yield_to_maturity_type: Whether the annual rate is the Annual Percentage Rate or the Effective Annual Rate.
+        bond_price: The current price of the bond in the market.
         discount_rate_per_period: Yield to Maturity is typically quoted annually. This is the exact rate per period.
         yield_to_maturity: The interest rate received if a bond is held to the maturity date.
 
@@ -29,7 +29,7 @@ class Bond(models.Model):
     face_value = models.DecimalField(max_digits=20, decimal_places=2)
     maturity = models.DecimalField(max_digits=20, decimal_places=4)
     coupon_rate = models.DecimalField(max_digits=5, decimal_places=4)
-    bond_price = models.DecimalField(max_digits=10, decimal_places=2)
     yield_to_maturity_type = models.CharField(max_length=100)
+    bond_price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_rate_per_period = models.DecimalField(max_digits=10, decimal_places=4)
     yield_to_maturity = models.DecimalField(max_digits=5, decimal_places=4)
