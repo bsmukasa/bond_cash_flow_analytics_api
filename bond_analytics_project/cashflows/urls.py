384d073e0b5b593ef3b9159ad80dc136b3574665
bsmukasa <bsmukasa@gmail.com>
2015-11-24 20:38:10 -0500

    Add url pattern for adding bond.

--- bond_analytics_project/cashflows/urls.py
+++ bond_analytics_project/cashflows/urls.py
@@ -1,1 +1,11 @@
+from django.conf.urls import url
 
+from bond_analytics_project.cashflows.views import BondValuation
+
+urlpatterns = [
+    url(
+        regex=r'^add/',
+        view=BondValuation.as_view(),
+        name='addBond'
+    ),
+]
