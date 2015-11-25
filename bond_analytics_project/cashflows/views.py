f764b1241a6eda1616d1e4536c2f331e6c3a95e3
bsmukasa <bsmukasa@gmail.com>
2015-11-24 20:09:28 -0500

    Add yield_maturity_function.

--- bond_analytics_project/cashflows/views.py
+++ bond_analytics_project/cashflows/views.py
@@ -35,11 +35,17 @@ class BondValuationYield(View):
             bond_price=request_dict['bond_price']
         )
 
-        new_bond.discount_rate_per_period = discount_rate_per_period(
+        period_discount_rate = discount_rate_per_period(
             nper=request_dict['maturity'],
             pv=request_dict['bond_price'],
             fv=request_dict['face_value'],
             coupon=request_dict['coupon_rate']
+        )
+
+        yield_maturity = yield_to_maturity(
+            yield_to_maturity_type=request_dict['yield_to_maturity_type'],
+            period_discount_rate=period_discount_rate,
+            coupon_payment_frequency=request_dict['coupon_payment_frequency']
         )
 
         new_bond.save()
@@ -57,7 +63,7 @@ def discount_rate_per_period(nper, pv, fv, coupon):
         nper: The number of payment periods in an annuity.
         pv: The present value or current bond price.
         fv: The future value or face value of the bond.
-        coupon: he stated annual interest rate.
+        coupon: The stated annual interest rate.
 
     Returns:
         The discount rate per period as a float.
@@ -66,3 +72,31 @@ def discount_rate_per_period(nper, pv, fv, coupon):
     pmt = np.pmt(rate, nper, pv, fv)
 
     return np.rate(nper, pmt, pv, fv)
+
+
+def yield_to_maturity(yield_to_maturity_type, period_discount_rate, coupon_payment_frequency):
+    """ Calculates the Yield to Maturity of the bond.
+
+    When Yield to Maturity is calculated as the Annual Percentage Rate, it is simply the Discount Rate per
+    Period multiply with the Coupon Payment Frequency. This is basically the discount rate per annum. When
+    the Effective Annual Rate is required, the following formula is used:
+
+        EXP(Coupon Payment Frequency*LN(Discount Rate per Period+1))-1
+        *EXP – Exponent function*
+        *LN – Natural Logarithm function*
+
+    Args:
+        yield_to_maturity_type: Whether the annual rate is the Annual Percentage Rate or the Effective Annual Rate.
+        period_discount_rate: The discount rate per period of the bond.
+        coupon_payment_frequency: Whether the coupon is paid annually or semi-annually.
+
+    Returns:
+        The yield to maturity as a float.
+    """
+    if yield_to_maturity_type == 1:
+        return period_discount_rate * coupon_payment_frequency
+    elif yield_to_maturity_type == 2:
+        x = period_discount_rate + 1
+        lan = np.log(x)
+        y = coupon_payment_frequency * lan
+        return np.exp(y) - 1
