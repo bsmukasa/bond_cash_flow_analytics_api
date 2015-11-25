bc6f519b284bfd4d6c9ac0f318620ffceeb4433d
bsmukasa <bsmukasa@gmail.com>
2015-11-24 20:40:49 -0500

    Install Django Rest Framework.

--- bond_analytics_project/bond_analytics_project/settings.py
+++ bond_analytics_project/bond_analytics_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'cashflows',
 )
 
@@ -101,3 +102,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
