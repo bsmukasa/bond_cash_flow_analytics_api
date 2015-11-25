3b36c6f30002455cd6a1cda3466d0ba4b934118f
bsmukasa <bsmukasa@gmail.com>
2015-11-24 20:38:37 -0500

    Add DjangoRestFramework url pattern.

--- bond_analytics_project/cashflows/urls.py
+++ bond_analytics_project/cashflows/urls.py
@@ -1,11 +1,22 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
+from rest_framework.routers import DefaultRouter
+
+from bond_analytics_project.cashflows import viewsets
 from bond_analytics_project.cashflows.views import BondValuation
+
+router = DefaultRouter()
+router.register(r'bonds', viewsets.BondViewSet)
 
 urlpatterns = [
     url(
         regex=r'^add/',
         view=BondValuation.as_view(),
         name='addBond'
+    ),
+
+    url(
+        regex=r'^api/v1/',
+        view=include(router.urls)
     ),
 ]
